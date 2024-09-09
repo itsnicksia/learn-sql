50614e45f13c78f635daf26fd8b318f38e7dd947
Nicholas Sia <fasterthoughts@gmail.com>
2024-09-09 22:23:17 +0800

    feat: add example puzzle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
-import { SQLConsole } from './SQLConsole.tsx'
+import {SQLConsole} from './SQLConsole.tsx'
 
 function App() {
   return (
     <>
-      <div style={{width: "800px"}}>
+      <div style={{width: "100%", height: "800px"}}>
+        <h1>Act 1: Learning SQL</h1>
+        <h2>Run a query that just returns a single row with a single column that has the value "1".</h2>
         <SQLConsole/>
       </div>
     </>
